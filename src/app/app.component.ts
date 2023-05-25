2a374173e2be51778bc5a111a36a88d73a3c67ec
Yiding Wang <yiding.wang82@gmail.com>
2023-05-25 10:45:09 -0400

    angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Address } from './address';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularExcise';
+
+  addresslist:Address[]=[];
+  newAddress:Address={name:"",naddress:""};
+
+  addNewAddress(){
+    this.addresslist.push(this.newAddress);
+    this.newAddress={name:"",naddress:""};
+  }
+
+  deleteAddress(i:number){
+    this.addresslist.splice(i,1);
+  }
+
+
 }
